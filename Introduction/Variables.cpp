51ecd1cf02880051c26e22d01cc5027813cc94d0
narayandhakal09 <narayandhakal443@gmail.com>
2022-04-26 21:30:35 +0545

    Variables
    All types of variables and its declaration and initilization methods

---
+++ Introduction/Variables.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // declaring the variable
+
+    int number;
+    double decimal;
+    char character;
+    string name;
+    bool what;
+    const float pi = 3.2;
+
+    // initilization of variables
+    // type variableName = value;
+
+    int age = 21;      // Integer inmbers
+    double salary = 1.2; // Double for decimal numbers
+    char character1 = 'c'; // For character use single quote
+    string name = "sanzgrapher";
+    bool result = true; // Bol for true or false
+
+    // Displaying variables
+    cout << "I am " << name << " My age is " << number << " is a " << bool(what) << " statement\n";
+}
