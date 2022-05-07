7d8675793232fe8164ddbb453de5854912b1cf6f
Narayan Dhakal <narayandhakal443@gmail.com>
2022-05-07 12:15:26 +0545

    Area and volume
    using class and objects to Get and print area and volume

---
+++ oop in cpp/Classes/Area-volume-of-room.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+class room
+{
+public:
+    double length;
+    double breadth;
+    double height;
+    // double volume;
+    // double area;
+    double getarea()
+    {
+        // area = length * breadth;
+        return length * breadth;
+    }
+    double getvolume()
+    {
+        return length * breadth * height;
+    }
+   
+};
+int main()
+{
+    room r1;
+    r1.length = 100;
+    r1.breadth = 85;
+    r1.height = 65;
+    r1.getarea();
+    r1.getvolume();
+    cout << "the volume of the room is : " << r1.getvolume() << endl;
+    cout << "the volume of the room is : " << r1.getarea() << endl;
+
+    return 0;
+}
