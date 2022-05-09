fb9125b741e5d532baf6b198a2d44ecd552dce86
Narayan Dhakal <narayandhakal443@gmail.com>
2022-05-09 15:39:34 +0545

    Constructors setup and first program

---
+++ oop in cpp/Constructors/constructors.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+
+class Line
+{
+public:
+    void setLength(double len);
+    double getLength(void);
+    Line(); // This is the constructor
+private:
+    double length;
+};
+
+// Member functions definitions including constructor
+Line::Line(void)
+{
+    cout << "Object is being created" << endl;
+}
+void Line::setLength(double len)
+{
+    length = len;
+}
+double Line::getLength(void)
+{
+    return length;
+}
+
+// Main function for the program
+int main()
+{
+    cout << "Before"<< endl;
+
+    Line line;
+    cout << "aftere" << endl;
+
+    // set line length
+    line.setLength(6.0);
+    cout << "Length of line : " << line.getLength() << endl;
+
+    return 0;
+}
---
+++ oop in cpp/Constructors/readme.md
@@ -0,0 +1,11 @@
+ # constructors
+ - [constructors](#constructors)
+
+
+
+# problems
+| Problems         | Description                |View source code                    |
+| -----------      | -----------                         | -----------                         |
+| First Constructor   | Creating  Constructors   |[View](constructors.cpp)    |
+| Smart phones using constructors   | Creating  Constructors   |[View](smartphone-constructors.cpp)    |
+
