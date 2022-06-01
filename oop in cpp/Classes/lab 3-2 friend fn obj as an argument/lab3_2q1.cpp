86f5c3472c933c859261d32140bd5751836bfa8a
Narayan Dhakal <narayandhakal443@gmail.com>
2022-06-01 21:21:26 +0545

    lab 3.2 starts

---
+++ oop in cpp/Classes/lab 3-2 friend fn obj as an argument/lab3_2q1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>  
+using namespace std;
+class XYZ{
+    int num1;
+    int getNum1();
+};
+int XYZ::getNum1(){
+    return 0;
+}
+class ABC{
+    int num2;
+    int getNum2();
+};
+int ABC::getNum2{
+
+    return 0;
+}
+int max( ){
+
+}
+
+int main() 
+{
+    
+    return 0;
+}
