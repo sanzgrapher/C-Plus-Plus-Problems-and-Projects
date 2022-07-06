da41a1a195457e6d28017eb677dba478d4371ccc
Narayan Dhakal <narayandhakal443@gmail.com>
2022-07-06 20:43:50 +0545

    static Function

--- oop in cpp/Classes/Practice/staticfn.cpp
+++ oop in cpp/Classes/Practice/staticfn.cpp
@@ -12,27 +12,38 @@ public:
 };
 void test::setno()
 {
+    cout << "before"<<no << endl;
+    cout << "count : " << count << endl;
+
     no = ++count;
+    cout << "count : " << count << endl;
+
+    cout << "after" << no << endl;
 }
 void test::dispno()
-{
+{ 
     cout << "obj no : " << no << endl;
 }
   void test::dispcount()
 {
-    cout << "count : " << count;
+    cout << "count : " << count<<endl;
 }
 
 int test::count;
 int main()
 {
     test t1, t2;
-    t1.setno();
-    t2.setno();
-
-    t2.dispcount();
-
     test::dispcount();
+    
+    t1.dispno();
+    t1.setno();
+    t1.dispcount();
+    t2.setno();
+    // t2.dispcount();
+
+    // t2.dispcount();
+
+    // test::dispcount();
 
     return 0;
 }
