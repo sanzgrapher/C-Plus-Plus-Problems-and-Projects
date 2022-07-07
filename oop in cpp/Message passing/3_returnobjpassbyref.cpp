7570c9580d9c313f0d40e7cbd84e48adc6d689fe
Narayan Dhakal <narayandhakal443@gmail.com>
2022-07-07 12:04:24 +0545

    return obj using pass by reference using pointer

---
+++ oop in cpp/Message passing/3_returnobjpassbyref.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+class Demo
+{
+    int a;
+
+public:
+    void setdata(int x)
+    {
+        a = x;
+    }
+    Demo square(Demo *p)
+    {
+        Demo y;
+        y.a = p->a * p->a;
+        return y;
+    }
+
+    void putdata()
+    {
+        cout << a;
+    }
+};
+int main()
+{
+    Demo o1, o3;
+    o1.setdata(10);
+    o3 = o3.square(&o1);
+    o3.putdata();
+}
