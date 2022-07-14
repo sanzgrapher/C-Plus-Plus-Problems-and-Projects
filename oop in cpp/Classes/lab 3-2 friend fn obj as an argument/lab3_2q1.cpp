47c52e69f86bef0736d6f475d7a4f017ec8aa2a2
Narayan Dhakal <narayandhakal443@gmail.com>
2022-07-14 19:55:47 +0545

    Friend function and obj as argument

--- oop in cpp/Classes/lab 3-2 friend fn obj as an argument/lab3_2q1.cpp
+++ oop in cpp/Classes/lab 3-2 friend fn obj as an argument/lab3_2q1.cpp
@@ -1,26 +1,45 @@
 #include <iostream>  
 using namespace std;
-class XYZ{
-    int num1;
-    int getNum1();
-};
-int XYZ::getNum1(){
-    return 0;
-}
+class XYZ; // Forward declaration needed for line no :11 as argument
 class ABC{
-    int num2;
-    int getNum2();
-};
-int ABC::getNum2{
+    int a1;
 
-    return 0;
+    public:
+    void getValue(int val){
+        a1 = val;
+    }
+    friend void max(ABC a, XYZ x);  //friend function with arguments as objects
+};
+class XYZ{
+    int x1;
+
+    public:
+    void getValue(int val){
+        x1 = val;
+    }
+    friend void max(ABC a,XYZ x); // same as 11
+};
+
+void max(ABC a, XYZ x) // takes argument as objects
+{
+if (a.a1>x.x1){
+    cout << "class ABC a1 ";
+}else{
+    cout << "class XYZ x1 ";
+
 }
-int max( ){
 
 }
 
 int main() 
 {
-    
+
+    ABC a2;
+    a2.getValue(5); // change value to alter result
+    XYZ x2;
+    x2.getValue(12); // change value to alter result
+
+    max(a2, x2); // calling function passing arguments
+
     return 0;
 }
